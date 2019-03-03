01cf5984ebf7975baf386aae9622c1adc755243e
Tyler Marenger <tjmareng@mtu.edu>
2019-03-02 21:47:07 -0500

    Implemented Error Handling

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Ange
 
 // const argv = yargs
 //   .options({
-    
+
 //     // ADDRESS
 //     a: {
 //       demand: true,
@@ -17,7 +17,7 @@ const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Ange
 //       describe: 'Address to fetch weather for',
 //       string: true
 //     }
-  
+
 // })
 //   .help()
 //   .alias('help', 'h')
@@ -30,15 +30,38 @@ request({
   url: darkskyURL,
   json: true
 }, (error, response, body) => {
+  if (error) {
+    // Low-level error 
+    console.log('Unable to connect to the weather service. ')
+  
+  } else if (response.body.error) {
+    // Invalid location -- URL error
+    console.log('Unable to find location. Please try again with a valid location. ')
+  
+  } else {
     console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain. \n');
+  
+  }
 });
 
 // GEOCODING
 request({
   url: geocodeURL,
   json: true
-}, (error, response) =>{
-  const latitude = response.body.features[0].center[0];
-  const longitude = response.body.features[0].center[1];
-  console.log(latitude, longitude);
+}, (error, response) => {
+  if (error) {
+    // Low-level error 
+    console.log('Unable to connect to location services. ')
+
+  } else if (response.body.features.length === 0) {
+    // Invalid location -- URL error
+    console.log('Unable to find location. Please try again with a valid location. ')
+
+  } else {
+    // Find and print latitude and longitude 
+    const latitude = response.body.features[0].center[0];
+    const longitude = response.body.features[0].center[1];
+    console.log(latitude, longitude);
+  }
+
 })
