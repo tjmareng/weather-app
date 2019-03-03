4f3c69bd23bb221d4574bf5ba77a2bbbb1c27ed8
Tyler Marenger <tjmareng@mtu.edu>
2019-03-02 21:35:54 -0500

    Using Mapbox Geocoding API -- Latitude/Longitude

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const request = require('request');
 const yargs = require('yargs');
 
-const url = 'https://api.darksky.net/forecast/KEY';
+// DARKSKY API
+const darkskyURL = 'https://api.darksky.net/forecast/9a06b0fc0575fe9a8a56720eb9d953f6/37.8267,-122.4233';
+
+// GEOCODING --- MAPBOX API
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGptYXJlbmciLCJhIjoiY2pzc2FtdXQ1MDEwMzN5dGxjdWpzem1sdSJ9.snBdNxYv3k0S3gzbvsF8BA';
+
 // const argv = yargs
 //   .options({
     
@@ -20,9 +25,20 @@ const url = 'https://api.darksky.net/forecast/KEY';
 
 // console.log(argv);
 
+// FORCAST
 request({
-  url: url
+  url: darkskyURL,
+  json: true
 }, (error, response, body) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain. \n');
 });
+
+// GEOCODING
+request({
+  url: geocodeURL,
+  json: true
+}, (error, response) =>{
+  const latitude = response.body.features[0].center[0];
+  const longitude = response.body.features[0].center[1];
+  console.log(latitude, longitude);
+})
